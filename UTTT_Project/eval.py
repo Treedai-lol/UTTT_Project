00c437ba881ba7317a0dd8c492686fcd78d11772
treedai <treedaimail>
2025-12-31 09:44:26 +0800

    completed NaiveEval function and added pseudocode of the minimax function

--- UTTT_Project/eval.py
+++ UTTT_Project/eval.py
@@ -16,14 +16,31 @@ def GetMoves(board:lib.Board) ->list:
 def DumbEval(board:lib.Board,o:bool) ->int:
     return 0
 def NaiveEval(board:lib.Board,o:bool) ->int:
+    temp = 0
     wb = board.wonboards
-    
-
-
+    if o:
+        for i in wb:
+            if i == 1:
+                temp = temp+1
+            if i == 2:
+                temp = temp-1
+    else:
+        for i in wb:
+            if i == 1:
+                temp = temp-1
+            if i == 2:
+                temp = temp+1
+    return temp
+def Search(board:lib.Board,o:bool,depth:int):
+    while depth>0:
+        wl = GetMoves(board)
+        for i in wl:
+            print(i)
+            #lib.MakeMove(board,o,i[0],i[1])
 
 def main():
     board = lib.BoardInit()
-    lib.StartRound(board,True)
-    print(GetMoves(board))
+    o = True
+    Search(board,o,1)
 if __name__ == '__main__':
     main()
