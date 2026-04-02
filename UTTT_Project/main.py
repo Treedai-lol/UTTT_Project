2d2180df5eafcb67d300bc3b4b8914729fabed00
unknown <ck11400040@gl.ck.tp.edu.tw>
2026-04-02 15:20:07 +0800

    Revamped MCTS Search

--- UTTT_Project/main.py
+++ UTTT_Project/main.py
@@ -1,8 +1,11 @@
-def func():
-    return []
+from copy import deepcopy
+from time import perf_counter
 
 def main():
-    print(func()==[])
-
+    t1 = perf_counter()
+    a = 10
+    for i in range(1000000):
+        b = deepcopy(a)
+    print(perf_counter()-t1)
 if __name__ == '__main__':
     main()
--- UTTT_Project/oldeval.py
+++ UTTT_Project/oldeval.py
@@ -2,13 +2,11 @@
 import math
 import random
 import lib
-import neural
 from time import perf_counter
-from copy import deepcopy
 
 
 class MCTSNode():
-    def __init__(self, board:lib.Board, prob:float,parent=None, move=None, player=None):
+    def __init__(self, board:lib.Board, parent=None, move=None, player=None):
         self.board = board
         self.parent = parent
         self.move = move
@@ -16,20 +14,21 @@ class MCTSNode():
         self.children = []
         self.visits = 0
         self.wins = 0.0
+        self.untried_moves = GetMoves(board)
         self.terminal = (GetMoves(board)==[])
-        self.prob = prob
     def is_fully_expanded(self):
         return len(self.untried_moves) == 0
     
     def expand(self):
-        temp = NaiveEval(self.board).pop(0)
-        j = 0
-        prevsb = deepcopy(self.board.sb)
-        for i in GetMoves(self.board):
-            self.board.MakeMove(i[0],i[1])
-            self.children.append(MCTSNode(self.board,self,i,self.board.o,temp[j]))
-            j+=1
-            self.board.UnmakeMove(i[0],i[1],prevsb)
+        move = self.untried_moves.pop()
+        new_board = self.board.copy()
+        new_board.MakeMove(new_board.o,move[0],move[1])
+        new_board.o = not new_board.o
+
+        child = MCTSNode(new_board, parent=self, move=move, player=self.board.o)
+        self.children.append(child)
+        return child
+    
     def best_child(self, c=1.4):
         for child in self.children:
             if child.visits == 0:
@@ -42,11 +41,25 @@ class MCTSNode():
 
         return max(self.children, key=ucb)
     
-    def rollout(self)->list: #returns the average amount of wins for O(0 to 1)
-        return NaiveEval(self.board)[0]
-    def backpropagate(self, wins:float):
+    def rollout(self): #uses random moves to finish the game, True or False for a win, None for a draw
+        board = self.board.copy()
+        while True:
+            o = board.o
+            moves = GetMoves(board)
+            if not moves:
+                return None
+            move = random.choice(moves)
+            board.MakeMove(o,move[0],move[1])
+            if board.GameFinished(o) is True:
+                return o
+            board.o = not board.o
+    def backpropagate(self, winner):
         self.visits += 1
-        self.wins+=wins
+        if self.player is not None:
+            if winner is None:
+                self.wins += 0.5
+            elif winner == self.player:
+                self.wins += 1.0
         if self.parent:
             self.parent.backpropagate(winner)
 def mcts_search(root_board, iterations=500):
@@ -59,26 +72,55 @@ def mcts_search(root_board, iterations=500):
         if not node.board.GameFinished(not node.board.o) and not node.is_fully_expanded() and not node.terminal:
             node = node.expand()
         
-        wins = node.rollout()
+        if node.board.GameFinished(not node.board.o):
+            winner = node.board.o
+        elif node.terminal:
+            winner = None
+        else:    
+            winner = node.rollout()
 
-        node.backpropagate(wins)
+        node.backpropagate(winner)
     best = max(root.children, key=lambda c: c.visits)
     return best.move
+def mcts_eval(root_board, iterations=500)->int:
+    root = MCTSNode(root_board, player=False)
+    for _ in range(iterations):
+        node = root
+        while not node.board.GameFinished(not node.board.o) and node.is_fully_expanded() and not node.terminal:
+            node = node.best_child()
+
+        if not node.board.GameFinished(not node.board.o) and not node.is_fully_expanded() and not node.terminal:
+            node = node.expand()
+        
+        if node.board.GameFinished(not node.board.o):
+            winner = node.board.o
+        elif node.terminal:
+            winner = None
+        else:    
+            winner = node.rollout()
+
+        node.backpropagate(winner)
+        
+    eval = root.wins/root.visits*100-50
+    return eval
     
 def RandomEval(board:lib.Board) ->list:
     return random.choice(GetMoves(board))
-def NaiveEval(board:lib.Board) ->list:
-    output = []
+def NaiveEval(board:lib.Board,o:bool) ->int:
+    temp = 0
     wb = board.wonboards
-    for i in wb:
-        if i == 1:
-            temp+=1
-        if i == 0:
-            temp+=0.5
-    temp/=9
-    output.append(temp)
-    for i in GetMoves(board):
-        output.append(1/len(GetMoves))
+    if o:
+        for i in wb:
+            if i == 1:
+                temp = temp+1
+            if i == 2:
+                temp = temp-1
+    else:
+        for i in wb:
+            if i == 1:
+                temp = temp-1
+            if i == 2:
+                temp = temp+1
     return temp
 def GetMoves(board: lib.Board) ->list:
     movelist = []
